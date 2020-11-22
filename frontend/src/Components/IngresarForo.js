2f621bf299a82332adb0cf2b852badf5669d7c21
Your NameAnyi <lorena0118a@gmail.com>
2020-11-22 18:47:16 -0500

    services connection

---
+++ frontend/src/Components/IngresarForo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import Navigation from "./Navigation";
+import BotonBack from "./BotonBack";
+
+const IngresarForo = () => {
+  return (
+    <div className="container">
+
+
+    </div>
+  );
+};
+export default withRouter(IngresarForo);
